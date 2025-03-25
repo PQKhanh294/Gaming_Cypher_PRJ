f1b4a7341319ddb980e14f031502e48f82bfece2
vohai04 <162089201+vohai04@users.noreply.github.com>
2025-03-25 20:34:16 +0700

    Update Computer.java

--- Computer.java
+++ Computer.java
@@ -1,13 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package model;
 
 /**
  *
  * @author haivo
  */
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
 
 public class Computer {
     private int id;
