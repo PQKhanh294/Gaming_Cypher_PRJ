058e863138495e1698eacad3efd816b09a3811d2
PQKhanh294 <161905750+PQKhanh294@users.noreply.github.com>
2025-03-25 16:12:02 +0700

    Update LoginServlet.java

--- LoginServlet.java
+++ LoginServlet.java
@@ -2,53 +2,47 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
+package controller;
 
-package Controller;
-
-import DAO.CustomerAccountDAO;
+import dao.LoginDAO;
+import dao.UserDAO;
 import java.io.IOException;
-import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-import model.CustomerAccount;
+import java.util.List;
+import model.Account;
 
 /**
  *
- * @author ASUS
+ * @author TU
  */
+@WebServlet(name = "LoginServlet", urlPatterns = {"/Login"})
 public class LoginServlet extends HttpServlet {
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet LoginServlet</title>");  
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet LoginServlet at " + request.getContextPath () + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    } 
+
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -56,7 +50,7 @@ public class LoginServlet extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         String action = request.getParameter("action");
         if ("logout".equalsIgnoreCase(action)) {
             // Xóa session
@@ -66,19 +60,20 @@ public class LoginServlet extends HttpServlet {
             }
 
             // Xóa cookie email
-            Cookie emailCookie = new Cookie("userEmail", "");
+            Cookie emailCookie = new Cookie("user", "");
             emailCookie.setMaxAge(0); // Xóa cookie ngay lập tức
             emailCookie.setPath("/");
             response.addCookie(emailCookie);
 
             // Chuyển hướng về trang login
-            response.sendRedirect("index.html");
+            response.sendRedirect("home.jsp");
             return;
         }
-    } 
+    }
 
-    /** 
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -86,32 +81,37 @@ public class LoginServlet extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-       String email = request.getParameter("email");
-        String password = request.getParameter("pass");
-
-        // Gọi phương thức kiểm tra tài khoản từ database
-        CustomerAccount customer = CustomerAccountDAO.validateLogin(email, password);
-
-        if (customer != null) {
-            // Đăng nhập thành công -> Lưu session và chuyển hướng đến FruitServlet
-            HttpSession session = request.getSession();
-            Cookie emailCookie = new Cookie("userEmail", email);
-            emailCookie.setMaxAge(7 * 24 * 60 * 60); // 7 ngày
-            emailCookie.setPath("/");
-            response.addCookie(emailCookie);
-            
-            session.setAttribute("loggedInUser", customer);
-            
-            response.sendRedirect("home.jsp?status=success");
+            throws ServletException, IOException {
+        String user = request.getParameter("user");
+        String pass = request.getParameter("pass");
+        String remember = request.getParameter("remember");
+        LoginDAO dao = new LoginDAO();
+        Account a = dao.login(user, pass);
+        if (a == null) {
+            request.setAttribute("errorMessage", "Wrong username or password. Try again!");
+            request.getRequestDispatcher("login.jsp").forward(request, response);
         } else {
-            // Đăng nhập thất bại -> Quay lại login.jsp với thông báo lỗi
-            response.sendRedirect("login.jsp?error=1");
+            if (remember != null && remember.equals("1")) {
+                Cookie emailCookie = new Cookie("user", user);
+                emailCookie.setMaxAge(7 * 24 * 60 * 60); // 7 ngày
+                emailCookie.setSecure(false); // Để đảm bảo lưu được trên HTTP
+                emailCookie.setPath("/"); // Đặt cookie có hiệu lực cho toàn bộ ứng dụng
+                response.addCookie(emailCookie);
+            }
+            
+            UserDAO userDAO = new UserDAO();
+            List<Account> userList = userDAO.getAllUsers();  // Lấy danh sách user từ DB
+            
+            HttpSession session = request.getSession();
+            session.setAttribute("acc", a);
+            session.setAttribute("userList", userList);
+            request.getRequestDispatcher("home.jsp").forward(request, response);
         }
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
